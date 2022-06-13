2a99bd5c7a23891ab9c0b06300374deaa66d3c06
nabeelcodes <nabeeldesigner91@hotmail.com>
2022-06-13 21:20:03 +0530

    Repurposed meta tags in document.js

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -5,21 +5,13 @@ export default function Home() {
     <>
       <Head>
         <title>Web Bulletin</title>
-        <meta name="description" content="A simple and fast web blog built with NextJS and Strapi CMS. " />
-        {/* <meta property="og:image" content="https://i.imgur.com/Jtl3tJG.png" /> */}
         <meta property="og:title" content="Web Bulletin"></meta>
-        <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <h1>Web Bulletin Blog Using Next and Strapi</h1>
       <a href="https://www.google.com" target="_blank" rel="noreferrer">
         this is a link to google
       </a>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati ea non unde repellat repudiandae. Itaque, quod! Mollitia placeat
-        perspiciatis non eos, totam deserunt quia, aspernatur explicabo qui delectus praesentium numquam aliquid ducimus ex eveniet unde similique
-        quas dolorum suscipit fugiat molestiae aliquam culpa. Expedita necessitatibus molestias earum repellat neque labore?
-      </p>
     </>
   );
 }
