333d2881f019d419d86bad16068a95c73a329a0f
nabeelcodes <nabeeldesigner91@hotmail.com>
2022-06-20 01:09:03 +0530

    Added blog-list on the main page
    - added proper height for 404 page
    - added `strapiApiData.json` to refer to api response data
    - `strapiApiData.json` is in gitinore and not ushed to remote

--- src/pages/404.jsx
+++ src/pages/404.jsx
@@ -1,15 +1,15 @@
 import { Container } from '../styles/utilities';
 
 const cssStyles = {
-  display: 'grid',
-  placeContent: 'center',
-  height: '74.1vh'
+	display: 'grid',
+	placeContent: 'center',
+	height: 'calc(100vh - 170px)'
 };
 
 export default function PageNotFound() {
-  return (
-    <Container style={cssStyles} width="70%">
-      <h1>404, not found my bruh !!</h1>
-    </Container>
-  );
+	return (
+		<Container style={cssStyles} width='70%'>
+			<h1>404, not found my bruh !!</h1>
+		</Container>
+	);
 }
