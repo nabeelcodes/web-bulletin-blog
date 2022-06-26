4384f5708cbbff31cab6f7404c1c786cdb6be24b
nabeelcodes <nabeeldesigner91@hotmail.com>
2022-06-27 00:05:58 +0530

    Updated google fonts in _Document.jsx

--- src/pages/_document.jsx
+++ src/pages/_document.jsx
@@ -3,30 +3,29 @@ import { ServerStyleSheet } from 'styled-components';
 
 export default class MyDocument extends Document {
 	static async getInitialProps(ctx) {
-    const sheet = new ServerStyleSheet();
-    const originalRenderPage = ctx.renderPage;
+		const sheet = new ServerStyleSheet();
+		const originalRenderPage = ctx.renderPage;
 
-    try {
-      ctx.renderPage = () =>
-        originalRenderPage({
-          enhanceApp: (App) => (props) =>
-            sheet.collectStyles(<App {...props} />)
-        });
+		try {
+			ctx.renderPage = () =>
+				originalRenderPage({
+					enhanceApp: (App) => (props) => sheet.collectStyles(<App {...props} />)
+				});
 
-      const initialProps = await Document.getInitialProps(ctx);
-      return {
-        ...initialProps,
-        styles: (
-          <>
-            {initialProps.styles}
-            {sheet.getStyleElement()}
-          </>
-        )
-      };
-    } finally {
-      sheet.seal();
-    }
-  }
+			const initialProps = await Document.getInitialProps(ctx);
+			return {
+				...initialProps,
+				styles: (
+					<>
+						{initialProps.styles}
+						{sheet.getStyleElement()}
+					</>
+				)
+			};
+		} finally {
+			sheet.seal();
+		}
+	}
 
 	render() {
 		return (
@@ -34,7 +33,7 @@ export default class MyDocument extends Document {
 				<Head>
 					<link rel='preconnect' href='https://fonts.googleapis.com' />
 					<link rel='preconnect' href='https://fonts.gstatic.com' crossOrigin={+true} />
-					<link href='https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700;&display=swap' rel='stylesheet' />
+					<link href='https://fonts.googleapis.com/css2?family=Inter:wght@400;500;700&display=swap' rel='stylesheet' />
 					<link rel='icon' href='/favicon.ico' />
 					<meta name='description' content='A simple and fast web blog built with NextJS and Strapi CMS. ' />
 					{/* <meta property="og:image" content="https://i.imgur.com/Jtl3tJG.png" /> */}
