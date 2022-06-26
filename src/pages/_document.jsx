308ac88126b779ce4ecf83c1c74b763b01cbe6fb
nabeelcodes <nabeeldesigner91@hotmail.com>
2022-06-26 17:56:53 +0530

    Updated yarn version to latest stable

--- src/pages/_document.jsx
+++ src/pages/_document.jsx
@@ -1,23 +1,23 @@
 import Document, { Head, Html, Main, NextScript } from 'next/document';
 
 export default class MyDocument extends Document {
-  render() {
-    return (
-      <Html>
-        <Head>
-          <link rel="preconnect" href="https://fonts.googleapis.com" />
-          <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin={+true} />
-          <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700;&display=swap" rel="stylesheet" />
-          <link rel="icon" href="/favicon.ico" />
-          <meta name="description" content="A simple and fast web blog built with NextJS and Strapi CMS. " />
-          {/* <meta property="og:image" content="https://i.imgur.com/Jtl3tJG.png" /> */}
-        </Head>
+	render() {
+		return (
+			<Html lang='en'>
+				<Head>
+					<link rel='preconnect' href='https://fonts.googleapis.com' />
+					<link rel='preconnect' href='https://fonts.gstatic.com' crossOrigin={+true} />
+					<link href='https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700;&display=swap' rel='stylesheet' />
+					<link rel='icon' href='/favicon.ico' />
+					<meta name='description' content='A simple and fast web blog built with NextJS and Strapi CMS. ' />
+					{/* <meta property="og:image" content="https://i.imgur.com/Jtl3tJG.png" /> */}
+				</Head>
 
-        <body>
-          <Main />
-          <NextScript />
-        </body>
-      </Html>
-    );
-  }
+				<body>
+					<Main />
+					<NextScript />
+				</body>
+			</Html>
+		);
+	}
 }
