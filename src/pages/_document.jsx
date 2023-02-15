19a76cea5579733dc3fa67bbb1054cb71f6d4f56
nabeelcodes <nabeeldesigner91@hotmail.com>
2023-02-15 20:34:51 +0530

    Added Sitemaps and removed cdn link for NProgress
    - added exceptions in gitignore for sitemaps and robot.txt

--- src/pages/_document.jsx
+++ src/pages/_document.jsx
@@ -52,13 +52,6 @@ export default class MyDocument extends Document {
 						type='font/woff2'
 						crossOrigin='anonymous'
 					/>
-					<link
-						rel='stylesheet'
-						href='https://cdnjs.cloudflare.com/ajax/libs/nprogress/0.2.0/nprogress.min.css'
-						integrity='sha512-42kB9yDlYiCEfx2xVwq0q7hT4uf26FUgSIZBK8uiaEnTdShXjwr8Ip1V4xGJMg3mHkUt9nNuTDxunHF0/EgxLQ=='
-						crossOrigin='anonymous'
-						referrerPolicy='no-referrer'
-					/>
 				</Head>
 
 				<body>
